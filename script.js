b44ddabd0186f5fa74802ce02af3b7b654ee9a64
noellerjd <noellerninja@gmail.com>
2022-05-29 22:39:03 -0600

    posters in uniform size, description, title listed on search

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ var searchFormEl = document.querySelector('#search-form')
 var listEl = document.querySelector('#list')
 var baseUrl = 'https://imdb-api.com'
 
-
+// boilerplate request options for imbd api
 var requestOptions = {
     method: 'GET',
     redirect: 'follow'
 };
 
-
+// api search event
 var submitSearch = (event) => {
     event.preventDefault()
     
     if(searchEl.value == ''){ //will become a modal
         alert("Please enter a movie name first!");
-    } else{
+    } else{ //search, then log response, take the response and run renderResults function
         fetch(`https://imdb-api.com/API/SearchMovie/k_fp9014yi/${searchEl.value}`, 
         requestOptions)
         .then(response => {console.log(response); return response.json()})
@@ -30,10 +30,28 @@ var submitSearch = (event) => {
 submitButton.addEventListener('click', submitSearch);
 
 function renderResults(result) {
-    console.log(result);
-    console.log(result.results[0].title);
+    var listItem = document.createElement('h1');
+    var posterItem = document.createElement('img');
+    var descItem = document.createElement('p');
+    posterItem.className = "poster";
+    listItem.className = "movietitle";
+    descItem.className = "description";
+
+
     
-    var listItem = document.createElement('li')
+    // poster
+    posterItem.src = result.results[0].image
+    listEl.appendChild(posterItem)
+
+    // title
     listItem.textContent = result.results[0].title
     listEl.appendChild(listItem)
+
+    // Description
+    descItem.textContent = result.results[0].description
+    listEl.appendChild(descItem)
+
+
+    // console.log(result.results[0].title);
+    console.log(result);
 }
